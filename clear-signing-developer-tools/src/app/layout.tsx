6da51c9df3e63497c421e276f3874a0c2732b6bc
Laurence Lord <laurence.lord@ledger.fr>
2024-09-19 14:46:26 +0100

    Add required fonts

--- clear-signing-developer-tools/src/app/layout.tsx
+++ clear-signing-developer-tools/src/app/layout.tsx
@@ -1,17 +1,36 @@
 import "~/styles/globals.css";
 
-import { GeistSans } from "geist/font/sans";
+import { Inter, Roboto } from "next/font/google";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Clear Signing Preview",
 };
 
+const inter = Inter({
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-inter",
+  weight: ["400", "500"],
+});
+
+const roboto = Roboto({
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-roboto",
+  weight: ["400", "500"],
+});
+
 export default function RootLayout({
   children,
-}: Readonly<{ children: React.ReactNode }>) {
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
+    <html
+      lang="en"
+      className={`${roboto.className} ${roboto.variable} ${inter.variable}`}
+    >
       <body>{children}</body>
     </html>
   );
--- clear-signing-developer-tools/tailwind.config.ts
+++ clear-signing-developer-tools/tailwind.config.ts
@@ -1,12 +1,12 @@
 import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        sans: ["var(--font-roboto)"],
+        inter: ["var(--font-inter)"],
       },
     },
   },
