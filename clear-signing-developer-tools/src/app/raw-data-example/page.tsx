b80dd4294a3823102711fac61949f9292a94f64a
Laurence Lord <laurence.lord@ledger.fr>
2024-09-19 17:00:57 +0100

    Setup a shared type for PreviewData

--- clear-signing-developer-tools/src/app/raw-data-example/page.tsx
+++ clear-signing-developer-tools/src/app/raw-data-example/page.tsx
@@ -1,7 +1,12 @@
 import callData from "../../../../registry/paraswap/calldata-AugustusSwapper.json";
 
+export type PreviewData = {
+  intent: string;
+  displays: [{ label: string; displayValue: string }];
+};
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return
-const newMagicFunction = (foo: any) => foo;
+const newMagicFunction = (foo: any): PreviewData => foo;
 
 export default function Page() {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
