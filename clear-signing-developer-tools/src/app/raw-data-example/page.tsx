bbb889c349652f456a54575c030fd913b7db82eb
pierre-alexis.deville <pierre-alexis.deville@ledger.fr>
2024-09-20 14:00:34 +0200

    format augustus swapper

--- clear-signing-developer-tools/src/app/raw-data-example/page.tsx
+++ clear-signing-developer-tools/src/app/raw-data-example/page.tsx
@@ -1,17 +1,61 @@
+"use client";
+import { ERC7730Schema } from "~/types";
 import callData from "../../../../registry/paraswap/calldata-AugustusSwapper.json";
 
 export type PreviewData = {
   intent: string;
-  legalName: string;
-  owner: string;
   displays: [{ label: string; displayValue: string }];
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return
-const newMagicFunction = (foo: any): PreviewData => foo;
+interface DisplayItem {
+  label: string;
+  displayValue: string;
+}
+
+function extractDisplayFields(data: ERC7730Schema): DisplayItem[] {
+  const displays: DisplayItem[] = [];
+
+  const displayObject = data.display;
+  console.log(displayObject);
+
+  // Iterate through all the formats in display.formats
+  for (const formatKey in displayObject.formats) {
+    console.log("formatKey", formatKey);
+    const format = displayObject.formats[formatKey];
+
+    console.log("format", format);
+    // Iterate over the fields within each format
+    if (format?.fields && Array.isArray(format.fields)) {
+      format.fields.forEach((field) => {
+        let label: string | undefined;
+        const displayValue = field.path;
+
+        // If field has a $ref, extract label from definitions
+        if (field.$ref) {
+          const definitionKey = field.$ref.split(".").pop();
+          if (definitionKey && displayObject.definitions[definitionKey]) {
+            label = displayObject.definitions[definitionKey].label;
+          }
+        } else {
+          label = field.label;
+        }
+
+        // If both label and displayValue are available, push to displays array
+        if (label && displayValue) {
+          displays.push({ label, displayValue });
+        }
+      });
+    }
+  }
+
+  return displays;
+}
 
 export default function Page() {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const parsedData = newMagicFunction(callData);
-  return <pre>{JSON.stringify(parsedData, null, 2)}</pre>;
+  const parsedData = extractDisplayFields(callData as unknown as ERC7730Schema);
+  return (
+    <>
+      <pre>{JSON.stringify(parsedData, null, 2)}</pre>
+    </>
+  );
 }
