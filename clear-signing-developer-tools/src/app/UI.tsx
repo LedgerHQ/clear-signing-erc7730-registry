9448616806681463816e0a505b208031acd4f7e9
Laurence Lord <laurence.lord@ledger.fr>
2024-09-20 10:24:50 +0100

    Extract UI into a separate file

---
+++ clear-signing-developer-tools/src/app/UI.tsx
@@ -0,0 +1,31 @@
+import type { ReactElement } from "react";
+
+export const UI = {
+  BlueLink: ({ children, href }: { children: string; href: string }) => (
+    <a className="text-blue-500" href={href}>
+      {children}
+    </a>
+  ),
+  HeadingField: ({ children }: { children: string }) => (
+    <div className="mb-2 text-sm uppercase text-neutral-400">{children}</div>
+  ),
+  Heading1: ({ children }: { children: string }) => (
+    <h1 className="mb-10 text-3xl font-medium">{children}</h1>
+  ),
+  Option: ({ children, value }: { children: string; value: string }) => (
+    <option className="text-lg" value={value}>
+      {children}
+    </option>
+  ),
+  Select: ({
+    children,
+  }: {
+    children:
+      | ReactElement<HTMLSelectElement>
+      | ReactElement<HTMLSelectElement>[];
+  }) => (
+    <select className="border border-neutral-400 bg-white p-2 font-sans text-lg marker:-left-2 marker:hidden">
+      {children}
+    </select>
+  ),
+};
