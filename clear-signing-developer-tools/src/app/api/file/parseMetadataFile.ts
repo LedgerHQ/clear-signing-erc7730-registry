46e0b98f48a05a40178809932cd5be8f2b35c9a7
Laurence Lord <laurence.lord@ledger.fr>
2024-09-25 16:13:40 +0100

    Rename variables in parseMetadataFile

--- clear-signing-developer-tools/src/app/api/file/parseMetadataFile.ts
+++ clear-signing-developer-tools/src/app/api/file/parseMetadataFile.ts
@@ -6,24 +6,24 @@ export function parseMetadataFile(
   file: string,
   absolutePath: string,
 ): ERC7730Schema {
-  const json = JSON.parse(file) as ERC7730Schema;
+  const jsonMainFile = JSON.parse(file) as ERC7730Schema;
+  const includesValue = jsonMainFile.includes;
 
-  const includes = json.includes;
-  if (!includes) return json;
+  if (!includesValue) return jsonMainFile;
 
   const localFolderName = absolutePath.split("/").slice(0, -1).join("/");
-  const includedPath = path.join(localFolderName, includes);
-  const content: string = fs.readFileSync(includedPath, "utf-8");
+  const includesPath = path.join(localFolderName, includesValue);
+  const includesFileContent: string = fs.readFileSync(includesPath, "utf-8");
 
   try {
     const aggregatedJson = {
-      ...JSON.parse(content),
-      ...json,
+      ...JSON.parse(includesFileContent),
+      ...jsonMainFile,
     } as ERC7730Schema;
     delete aggregatedJson.includes;
     return aggregatedJson;
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (e) {
-    return json;
+    return jsonMainFile;
   }
 }
