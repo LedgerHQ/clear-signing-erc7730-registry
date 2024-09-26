d6e6c9e54a11a7e1e3402bd658a46d8a9b41f347
pierre-alexis.deville <pierre-alexis.deville@ledger.fr>
2024-09-26 09:26:52 +0200

    add deep merge

--- clear-signing-developer-tools/src/app/api/file/parseMetadataFile.ts
+++ clear-signing-developer-tools/src/app/api/file/parseMetadataFile.ts
@@ -2,6 +2,21 @@ import { type ERC7730Schema } from "~/types";
 import path from "path";
 import fs from "fs";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function deepMerge(target: any, source: any) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  for (const key of Object.keys(source)) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    if (source[key] instanceof Object && key in target) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      Object.assign(source[key], deepMerge(target[key], source[key]));
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return Object.assign(target || {}, source);
+}
+
 export function parseMetadataFile(
   file: string,
   absolutePath: string,
@@ -16,10 +31,10 @@ export function parseMetadataFile(
   const includesFileContent: string = fs.readFileSync(includesPath, "utf-8");
 
   try {
-    const aggregatedJson = {
-      ...JSON.parse(includesFileContent),
-      ...jsonMainFile,
-    } as ERC7730Schema;
+    const aggregatedJson = deepMerge(
+      JSON.parse(includesFileContent),
+      jsonMainFile,
+    ) as ERC7730Schema;
     delete aggregatedJson.includes;
     return aggregatedJson;
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
