4e48396ef8e3280d5426d23ee2e2843d59f80d34
Laurence Lord <laurence.lord@ledger.fr>
2024-09-29 11:31:01 +0100

    Style sign screen

--- clear-signing-developer-tools/src/app/screens/SignScreen.tsx
+++ clear-signing-developer-tools/src/app/screens/SignScreen.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import { Device } from "~/app/Device";
+import signButton from "./assets/sign-button.svg";
 
 export const SignScreen = ({
   owner,
@@ -7,7 +9,15 @@ export const SignScreen = ({
   owner: string;
   type: string;
 }) => (
-  <Device.OperationSummary>
-    {`Sign ${type} from ${owner}?`}
-  </Device.OperationSummary>
+  <>
+    <Device.OperationSummary>
+      {`Sign ${type} from ${owner}?`}
+    </Device.OperationSummary>
+    <div className="flex items-center justify-between p-4">
+      <Device.Heading>Hold to sign</Device.Heading>
+      <div className="border-light-grey flex h-[44px] w-[44px] items-center justify-center rounded-full border">
+        <Image src={signButton} alt="Sign" width={44} height={44} />
+      </div>
+    </div>
+  </>
 );
