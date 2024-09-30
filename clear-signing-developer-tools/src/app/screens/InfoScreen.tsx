9b65353287a19f983f2e42c0fc7e4f920395af07
Laurence Lord <laurence.lord@ledger.fr>
2024-09-30 16:59:02 +0100

    Style Info screen for Stax

--- clear-signing-developer-tools/src/app/screens/InfoScreen.tsx
+++ clear-signing-developer-tools/src/app/screens/InfoScreen.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
+import { useContext } from "react";
 import { Device } from "~/app/Device";
+import { DeviceContext } from "~/app/DeviceContext";
 import flexBackArrow from "~/app/screens/assets/flex-back-arrow.svg";
+import staxBackArrow from "~/app/screens/assets/stax-back-arrow.svg";
+import { cn } from "~/utils/cn";
 
 export const InfoScreen = ({
   address,
@@ -8,36 +12,60 @@ export const InfoScreen = ({
 }: {
   address: string;
   info: { lastUpdate: string; legalName: string; url: string };
-}) => (
-  <>
-    <div className="border-light-grey relative border-b">
-      <div className="absolute bottom-0 left-0 top-0 flex w-[52px] items-center justify-center">
-        <Image
-          className="inline-block w-5"
-          src={flexBackArrow}
-          alt="Back"
-          width={20}
-          height={20}
-        />
+}) => {
+  const isStax = useContext(DeviceContext) === "stax";
+
+  return (
+    <>
+      <div className="border-light-grey relative border-b">
+        <div
+          className={cn(
+            "absolute bottom-0 left-0 top-0 flex items-center justify-center",
+            isStax ? "w-[44px]" : "w-[52px]",
+          )}
+        >
+          {isStax ? (
+            <Image
+              className="inline-block w-4"
+              src={staxBackArrow}
+              alt="Back"
+              width={16}
+              height={16}
+            />
+          ) : (
+            <Image
+              className="inline-block w-5"
+              src={flexBackArrow}
+              alt="Back"
+              width={20}
+              height={20}
+            />
+          )}
+        </div>
+        <div
+          className={cn(
+            "py-[6px] text-center",
+            isStax ? "px-[44px]" : "px-[52px]",
+          )}
+        >
+          <Device.ActionText>Smart contract information</Device.ActionText>
+        </div>
       </div>
-      <div className="px-[52px] py-[6px] text-center">
-        <Device.ActionText>Smart contract information</Device.ActionText>
+      <div className="grow">
+        <Device.Section>
+          <Device.ActionText>Contract owner</Device.ActionText>
+          <Device.ContentText>
+            {legalName}
+            <br />
+            {url}
+          </Device.ContentText>
+        </Device.Section>
+        <Device.Section>
+          <Device.ActionText>Contract address</Device.ActionText>
+          <Device.ContentText>{address}</Device.ContentText>
+        </Device.Section>
       </div>
-    </div>
-    <div className="grow">
-      <Device.Section>
-        <Device.ActionText>Contract owner</Device.ActionText>
-        <Device.ContentText>
-          {legalName}
-          <br />
-          {url}
-        </Device.ContentText>
-      </Device.Section>
-      <Device.Section>
-        <Device.ActionText>Contract address</Device.ActionText>
-        <Device.ContentText>{address}</Device.ContentText>
-      </Device.Section>
-    </div>
-    <div></div>
-  </>
-);
+      <div></div>
+    </>
+  );
+};
