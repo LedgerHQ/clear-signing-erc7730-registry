4dd0769a2ec9f145c545290c92206dc360ce591d
Laurence Lord <laurence.lord@ledger.fr>
2024-10-04 12:58:14 +0100

    Ensure chosen operation is based on id or intent

--- developer-preview/src/app/DevicesDemo.tsx
+++ developer-preview/src/app/DevicesDemo.tsx
@@ -16,7 +16,10 @@ export const DevicesDemo = ({
   const { contract, metadata, operations, type } = data;
 
   const chosenOperation =
-    operations.find(({ id }) => selectedOperation === id) || operations[0];
+    operations.find(
+      ({ id, intent }) =>
+        selectedOperation === id || selectedOperation === intent,
+    ) ?? operations[0];
 
   if (!chosenOperation || contract.deployments.length < 1) return null;
 
