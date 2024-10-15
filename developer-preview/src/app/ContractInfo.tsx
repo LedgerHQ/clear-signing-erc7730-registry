13e0e95683eaaac603fc90acd483d6432d21e4c4
Laurence "DC5B" Lord <laurence.lord@ledger.fr>
2024-10-15 16:09:46 +0100

    Remove duplicate address links (#57)
    Co-authored-by: Frédéric Samier <frederic.samier@ledger.fr>

--- developer-preview/src/app/ContractInfo.tsx
+++ developer-preview/src/app/ContractInfo.tsx
@@ -1,11 +1,15 @@
 import { UI } from "~/ui/UI";
-import { type Deploymnent, type PreviewData } from "~/types/PreviewData";
+import { type PreviewData } from "~/types/PreviewData";
 import { formatShortAddress } from "~/app/formatShortAddress";
 
 export const ContractInfo = ({ data }: { data: PreviewData }) => {
   if (data.contract.deployments.length === 0) {
     return <UI.FauxInput error>No deployments found</UI.FauxInput>;
   }
+
+  const uniqueAddresses = Array.from(
+    new Set(data.contract.deployments.map(({ address }) => address)),
+  );
 
   return (
     <div>
@@ -14,7 +18,7 @@ export const ContractInfo = ({ data }: { data: PreviewData }) => {
           <UI.InputText>{data.contract.name}</UI.InputText>
 
           <div className="flex gap-5">
-            {data.contract.deployments.map(({ address }: Deploymnent) => (
+            {uniqueAddresses.map((address: string) => (
               <UI.GreyLink
                 href={`https://etherscan.io/address/${address}`}
                 key={address}
